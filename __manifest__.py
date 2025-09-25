a64065b95b3f260ef3360e59c52b8e65ad4c9713
Kevin Henrique <49871792+kevinbkt10@users.noreply.github.com>
2025-09-24 22:08:40 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Website Blog Clean URLs",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "depends": ["website", "website_blog"],
     "data": ["views/blog_templates.xml"],
     "post_init_hook": "post_init_create_redirects",  # remova esta linha se não quiser 301 automáticos
